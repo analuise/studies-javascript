44311e62d7557f2774ae9a8243258045715bf700
Ana Abreu <91298497+analuise@users.noreply.github.com>
2022-08-08 22:37:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var nome = "Ana Luise";
+var idade = 22;
+var idade2 = 10;
+var frase = "Brasil é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+
